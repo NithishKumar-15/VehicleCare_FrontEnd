df1ff68d0d39db8df3f777c33b910d604e175284
nithishkumarmurugesan2001@gmail.com <nithishkumarmurugesan2001@gmail.com>
2024-07-26 19:44:30 +0530

    Top level await

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2020', // Or any other target that supports top-level await
-  }
+  // build: {
+  //   target: 'es2020', // Or any other target that supports top-level await
+  // }
 })
 
