af8333cf48d600b8ca2e6fa762eff4ab77e8fd15
nithishkumarmurugesan2001@gmail.com <nithishkumarmurugesan2001@gmail.com>
2024-07-26 19:31:35 +0530

    Vite config changed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2019', // Or any other target that supports top-level await
+  }
 })
+
