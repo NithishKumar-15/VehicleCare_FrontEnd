8231c28d96cae15c04dc41204b8c6bfbec0ceae8
NithishKumar-15 <157941157+NithishKumar-15@users.noreply.github.com>
2024-07-26 21:22:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   target: 'es2020', // Or any other target that supports top-level await
-  // }
+  esbuild: {
+    target: 'esnext', // This sets the target to the latest JavaScript version
+  },
 })
 
